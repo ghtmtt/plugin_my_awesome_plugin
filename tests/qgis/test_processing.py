31c39a753bae141a791549e34f699e99d6c04b0c
Matteo Ghetta <matteo.ghetta@faunalia.it>
2025-07-09 09:05:15 +0200

    initial commit

---
+++ tests/qgis/test_processing.py
@@ -0,0 +1,40 @@
+#! python3  # noqa E265
+
+"""
+Usage from the repo root folder:
+
+.. code-block:: bash
+
+    # for whole tests
+    python -m unittest tests.qgis.test_plg_processing
+    # for specific test
+    python -m unittest tests.qgis.test_plg_processing.TestPlgprocessing.test_plg_processing_structure
+"""
+
+# PyQGIS
+from qgis.core import QgsApplication
+from qgis.testing import unittest, start_app
+
+from my_awesome_plugin.processing.provider import (
+    MyAwesomePluginProvider,
+)
+
+provider = None
+
+
+class TestProcessing(unittest.TestCase):
+    """Tests for processing algorithms."""
+
+    def setUp(self) -> None:
+        """Set up the processing tests."""
+        if not QgsApplication.processingRegistry().providers():
+            self.provider = MyAwesomePluginProvider()
+            QgsApplication.processingRegistry().addProvider(self.provider)
+        self.maxDiff = None
+                
+        # Start App needed to run processing on unittest
+        start_app()
+
+    def test_processing_provider(self):
+        """Sample test."""
+        print(f"Processing provider name : {self.provider.name()}")
